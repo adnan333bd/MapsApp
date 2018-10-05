6b4315d1ec41484e07f4abcab9ec02ef6b08a591
golam sobhani chowdhury <adnan333bd@gmail.com>
2018-10-05 21:58:09 +0600

    add list of places

--- App.js
+++ App.js
@@ -1,37 +1,53 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, TextInput } from 'react-native';
+import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import ListItem from './src/components/ListItem/ListItem';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-  'Double tap R on your keyboard to reload,\n' +
-  'Shake or press menu button for dev menu',
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component {
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   };
   placeNameChangedHandler = (value) => {
     this.setState({
       placeName: value
     });
   };
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() == "") {
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
+  };
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <ListItem key={i} placeName={place}  />
+    ));
+
     return (
-      <View style={styles.container}>
-        <Text>Place Name</Text>
-        <TextInput
-          style={{ width: 200, borderColor: 'gray', borderWidth: 1}}
-          placeholder="place name please"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler} />
-        <Text>Date</Text>
-        <TextInput
-          style={{ width: 200, borderColor: 'gray', borderWidth: 1}}
-          placeholder="place name please"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler} />
+      <View style={styles.container} >
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInputCommon}
+            placeholder="place name please"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler} />
+          <Button style={styles.buttonCommon} title="Add" onPress={this.placeSubmitHandler} />
+        </View>
+        <View>
+          {placesOutput}
+        </View>
       </View>
     );
   }
@@ -40,19 +56,26 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    paddingTop: 50,
+    padding: 20,
     justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'    
+    // backgroundColor: '#eee'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  textInputCommon: {
+    width: '70%',
+    //backgroundColor: '#bbb'
   },
+  buttonCommon: {
+    width: '30%',
+    //backgroundColor: '#999'
+  }
+
 });
---
+++ Readme.md
@@ -0,0 +1,26 @@
+## Getting started
+
+https://facebook.github.io/react-native/docs/getting-started.html
+
+react-native init yourproject
+
+cd yourproject
+
+react-native run-ios
+
+If you get an error when launching your app, please try these commands:
+
+    npm install --save-dev @babel/core
+    npm install --save-dev @babel/runtime
+
+## Styling cheat sheet
+
+If you want to get a full list about the supported properties, the following cheat sheet will be useful to you: https://github.com/vhpoet/react-native-styling-cheat-sheet
+
+If you want to learn more about Flexbox, don't miss this awesome article: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+
+##ios sim
+cmd + d for menu
+
+#android sim 
+ctrl m for menu
